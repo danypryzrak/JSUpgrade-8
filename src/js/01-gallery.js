0b1c87f5d62753a9a96a75304f90af3e2c91dda3
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-12-08 23:54:25 +0100

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createPicture = () => {
+    for (const galleryItem of galleryItems) {
+        const item = document.createElement('li')
+        item.classList.add('gallery__item')
+        item.innerHTML = `
+    <a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+    </a>`
+        gallery.append(item)
+    }
+}
+
+createPicture()
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+let time = localStorage.getItem("videoplayer-current-time")
+    
+
+player.on('timeupdate', throttle(function(ev) {
+    let currentTime = ev.seconds
+    localStorage.setItem("videoplayer-current-time", currentTime)
+}, 500)
+);
+
+
+
+if (time === null) {
+    return
+} else {
+    player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+}
