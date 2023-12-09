780994745b9fe4b99310c75e04344babd8a749de
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-12-09 16:37:42 +0100

    done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from "lodash.throttle";
 
+const feedbackForm = document.querySelector(".feedback-form")
+let formData = {}
+
+
+if (localStorage.getItem("feedback-form-state") === null) {
+    
+} else {
+    formData = JSON.parse(localStorage.getItem("feedback-form-state"))
+    feedbackForm.elements.email.value = formData.email
+    feedbackForm.elements.message.value= formData.message
+}
+
+feedbackForm.addEventListener("input", throttle(() => {
+    
+    formData.email = feedbackForm.elements.email.value;
+    formData.message = feedbackForm.elements.message.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    
+}, 500)
+)
+
+feedbackForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    localStorage.clear();
+    feedbackForm.elements.email.value = null
+    feedbackForm.elements.message.value = null
+})
